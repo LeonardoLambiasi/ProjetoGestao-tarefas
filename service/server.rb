f5f1c29e86a0a1156c3a1e646a76697df50ee384
Joselito Viveiros <joselito_nogueira@atlantico.com.br>
2014-11-08 16:48:45 -0300

    Criado tabela postit e refatoração para mural.

--- service/server.rb
+++ service/server.rb
@@ -1,18 +1,29 @@
 require 'sinatra'
+require 'sqlite3'
 
 set :port, 9090
 set :bind, '0.0.0.0'
+
+db = SQLite3::Database.new "dashboard.db"
+
+# Create a database
+rows = db.execute <<-SQL
+  create table if not exists postit (
+    id integer primary key autoincrement,
+    name varchar(255)
+  );
+SQL
 
 get '/' do
     redirect '/index.html'
 end
 
-get '/list' do
+get '/postits' do
     activities = File.open("dados.txt", "r"){ |file| file.read }
     activities
 end
 
-post '/add' do
+post '/postits' do
     @name = params[:name]
     File.open('dados.txt', 'a') do |f|
         f << @name << "\n"
